8de7008bdbde2a9008804d57474eaf51de882255
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2022-04-06 08:58:24 -0700

    Pointing the FHIR config to our dev fhir server

--- source/js/fhir-server-config.js
+++ source/js/fhir-server-config.js
@@ -18,55 +18,18 @@ export const fhirServerConfig = {
     //   ],
     //
     // }
-    { url: 'https://launch.smarthealthit.org/v/r3/fhir'},
-    { url: 'https://lforms-fhir.nlm.nih.gov/baseDstu3'},
-    { url: 'https://lforms-fhir.nlm.nih.gov/baseR4',
-      smartServiceUrl: 'https://lforms-smart-fhir.nlm.nih.gov/v/r4/fhir',
+    { url: 'https://hapi4-dev.lantanagroup.com/fhir',
       featuredQuestionnaires: [
         {
-          name: 'US Surgeon General family health portrait',
+          name: 'Some Other Questionnaire',
+          id: 'SYtL8Sw8',
+          code: 'SYtL8Sw8'
+        },
+        {
+          name: 'CareQuality Questionnaire',
           id: '54127-6-x',
           code: '54127-6'
-        },
-        {
-          name: 'Weight & Height tracking panel',
-          id: '55418-8-x',
-          code: '55418-8'
-        },
-        {
-          name: 'Comprehensive metabolic 1998 panel',
-          id: '24322-0-x',
-          code: '24322-0'
-        },
-        {
-          name: 'PHQ-9 quick depression assessment panel',
-          id: '44249-1-x',
-          code: '44249-1'
-        },
-        {
-          name: 'Hard Coronary Heart Disease (10-year risk)',
-          id: 'framingham-hchd-lhc'
-          //code: 'framingham-hchd'
-        },
-        {
-          name: 'Health Screening',
-          id: 'sdoh-health-screening'
-        },
-        {
-          name: 'Study drug toxicity panel',
-          id: 'study-drug-tox-x'
-          //code: 'study-drug-tox'
-        },
-        // {
-        //   name: 'Glasgow coma scale',
-        //   id: '35088-4-x',
-        //   code: '35088-4'
-        // },
-        {
-          name: 'AHC HRSN Screening',
-          id: 'lforms-ahn-hrsn-screening'
-        },
-
-    ]}
+        }]
+    }
   ]
 };
